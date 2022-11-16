d2dd7f9bc4e4dc51cca65b33e8626334b479f387
RiccardoRR57 <riccardo.rimondi44@gmail.com>
2022-11-16 10:23:16 +0100

    lezione 14/11/2022

---
+++ lab7/reali/divis_reale.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    float numeratore, denominatore;
+    cout << "Immettere due reali" << endl;
+    cin >> numeratore >> denominatore;
+    cout << numeratore << " / " << denominatore << " = " << numeratore/denominatore << endl;
+
+    return 0;
+}
+
