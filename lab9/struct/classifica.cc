9718eb3e148482718fbd3f2e618df807d5380cbc
RiccardoRR57 <riccardo.rimondi44@gmail.com>
2022-12-06 17:31:55 +0100

    lezione 5/12/2022

---
+++ lab9/struct/classifica.cc
@@ -0,0 +1,105 @@
+#include <iostream>
+
+using namespace std;
+
+const int LUNGH_NOME_MAX = 20;
+
+struct sciatore {
+        char nome[LUNGH_NOME_MAX];
+        int tempo_sec;
+    };
+
+int elenco_inserisci(sciatore elenco[], int lungh_elenco, char nome[], int lungh_max) {
+    if(lungh_elenco >= lungh_max) {
+        cout << "Elenco pieno!" << endl;
+        return 0;
+    }
+    cout << "elenco_inserisci: "<<nome<< "\n";
+
+    for(int i = 0; i < LUNGH_NOME_MAX && nome[i] != '\0'; i++) {
+        elenco[lungh_elenco].nome[i] = nome[i];
+    }
+
+    return lungh_elenco + 1;
+
+}
+
+void stampa_elenco(const sciatore elenco[], int lungh_elenco) {
+    cout << "Ci sono " << lungh_elenco << " partecipanti" << endl;
+
+    for (int j = 0; j < lungh_elenco; j++)
+    {
+        cout << "Partecipante " << j << ": " << elenco[j].nome << endl;
+    }
+}
+
+void stampa_vincitore(const sciatore elenco[], int lungh_elenco) {
+    cout << "Il vincitore è: " << sciatore1.nome << endl
+                << "Con un tempo di: " << sciatore1.tempo_sec / 60 << " minuti e " 
+                << sciatore1.tempo_sec % 60 << " secondi" << endl;
+                // TODO
+}
+
+int main() {
+    
+    const int SEC_IN_MIN = 60;
+    const int MAX_PARTECIPANTI = 20;
+
+    int num_partecipanti = 0, scelta;
+
+    sciatore partecipanti[MAX_PARTECIPANTI];
+
+    while(true) {
+
+        cout << "1.\tInserimento di un nuovo partecipante" << endl;
+        cout << "2.\tStampa numero partecipanti" << endl;
+        cout << "3.\tGara" << endl;
+        cout << "4.\tStampa primo classificato" << endl;
+        cout << "4.\tUscita" << endl;
+
+        cin >> scelta;
+
+        if(scelta == 4) break;
+
+        switch (scelta)
+        {  
+        case 1:
+            char nome[LUNGH_NOME_MAX];
+            cout << "Inserire il nome: ";
+            cin >> nome;
+            num_partecipanti = elenco_inserisci(partecipanti, num_partecipanti, nome, MAX_PARTECIPANTI);
+            break;
+
+        case 2:
+            stampa_elenco(partecipanti, num_partecipanti);
+            break;
+
+        case 3:
+
+            if(num_partecipanti == 0) {
+                cout << "Inserire almeno un partecipante!" << endl;
+            }
+
+            for(int j = 0; j < num_partecipanti; j++) {
+                
+                cout << "Inserire il tempo (min sec) di " << partecipanti[j].nome << ": ";
+                int min, sec;
+                cin >> min >> sec;
+                partecipanti[j].tempo_sec = min * SEC_IN_MIN + sec;
+            }            
+            break;
+        default:
+            break;
+        }
+    }
+
+
+
+    
+
+    
+    
+
+
+    
+}
